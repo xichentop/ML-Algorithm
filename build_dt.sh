373c2e0baa2a1ab3f1378e3317c28fea8c07f1f2
xichentop <johnxichen@gmail.com>
2017-01-27 12:32:45 -0800

    Delete build_dt.sh

--- build_dt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#executes decision tree learner and output accuracy results
-export JAVA_HOME=/opt/jdk1.8.0_60
-export PATH=/opt/jdk1.8.0_60/bin:$PATH
-javac *.java
-java DecisionTreeUtil02 $@
-#end
